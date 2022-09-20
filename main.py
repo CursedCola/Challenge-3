1cecdfae83f5bc34703c7729756802249cbc22dc
Marco Resendiz <113551818+maresendiz2@users.noreply.github.com>
2022-09-20 20:05:58 +0000

    push

--- main.py
+++ main.py
@@ -4,12 +4,20 @@
 #Day 3 Python Challenge
 
 # The task is as follows: You are going to create a program that first asks the user to enter text. It can be any text, an entire article, a paragraph, a sentence, a poem, whatever you want. Then the program will ask the user to enter three random letters. From that moment on, our code is going to process that information and result in five different types of analysis:
-text = input("")
+input1 = input("What is your favorite poem? ")
+input2 = input("Give us 3 random letters ")
 
+input2 = (" ")
+list = [input2]
+print(list)
 # 1. How many times each of those letters they have chosen appears. To achieve this, I advise you to store those letters in a list, and then use a method of strings that allows you to count how many times a substring appears within the string. One thing to keep in mind is that when searching for letters, there can be upper and lower case and this will affect the result. So, to make sure that absolutely all letters are counted, you should pass both the original text and the letters to be searched to lowercase.
 
-# 2. How many words are in the whole text? To achieve this part, remember that there is a string method that allows us to transform it into a list. And then there is a function that allows us to find out the length of a list.
 
+
+# 2. How many words are in the whole text? To achieve this part, remember that there is a string method that allows us to transform it into a list. And then there is a function that allows us to find out the length of a list.
+wrds_inText = [input1]
+print(wrds_inText)
+print(len(wrds_inText))
 # 3. What are the first and last letters of the text? Here, we will clearly use indexing.
 
 # 4. The system will show us how the text would look like if we inverted the order of the words. Is there any method that allows us to invert the order of a list? And another one that allows us to join these elements with spaces in between?
